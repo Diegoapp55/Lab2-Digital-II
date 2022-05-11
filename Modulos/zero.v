3838146dd7d2f8732e2d27d9b5f2f19f81e095b0
Diego Sánchez <Diegoapp55@gmail.com>
2022-05-11 11:23:25 -0500

    Agregada la correccion del profe
    Lo de solo poner clk en los always y no compuertas lógicas que "toquen" el reloj

--- Modulos/zero.v
+++ Modulos/zero.v
@@ -1,33 +1,31 @@
 `timescale 1ns / 1ps
 module zero(
   input [2:0] portA, //Por defecto, trabajar solo con el A
-  input init_isZero, //Enable de la operación
+  input init_isZero, //Enable de la operacion
   input clk,
   output comp        //Salida del comparador
 );
 reg [2:0] A;
 reg z;
 
-// A = portA;
 assign comp = z; /*Importante: assign solo se usa para cables (wire), usando la
                   sintaxis:
                   assign wire_name = value;
                   */
 
-always @ (posedge clk && init_isZero) begin
+always @ (posedge clk) begin
+  if (init_isZero) begin
   A = portA; /*Importante: Si se quiere asignar valores a registros, toca
                hacerlo dentro de un bloque always, porque para almacenar en un
-               registro se necesita algun flanco que habilite su carga y no
-               hacerlo a lo malditasea xD (regA = 20 sería hacerlo asíncrono,
-               entonces está mal)*/
-  // if (init_isZero) begin
+               registro se necesita algun flanco que habilite su carga(poner
+               regA = 20 fuera de un always seria hacerlo asincrono, entonces
+               esta mal)*/
     if (A == 0) begin
       z <= 1;
     end else begin
       z <= 0;
     end
-  // end
-
+  end
 end
 
 endmodule
